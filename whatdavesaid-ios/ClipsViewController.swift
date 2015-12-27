4613cc189aa6987b99eb107ccdd8f1ac47b24be2
Joe Fearnley <joe.fearnley@gmail.com>
2015-12-27 11:55:46 -0500

    Initial commit

---
+++ whatdavesaid-ios/ClipsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ClipsViewController.swift
+//  whatdavesaid-ios
+//
+//  Created by Joe Fearnley on 12/27/15.
+//  Copyright © 2015 Joe Fearnley. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ClipsViewController : UIViewController {
+    
+    var clips = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+}
