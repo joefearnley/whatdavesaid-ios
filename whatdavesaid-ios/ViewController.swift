81805c5fa48a3dcec177779beac804b81399ad13
Joe Fearnley <joe.fearnley@gmail.com>
2015-12-27 11:49:14 -0500

    Initial Commit

---
+++ whatdavesaid-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  whatdavesaid-ios
+//
+//  Created by Joe Fearnley on 12/27/15.
+//  Copyright © 2015 Joe Fearnley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
