428ef4b05f3c1e682936485e50259949c90deca5
Joe Fearnley <joe.fearnley@gmail.com>
2015-12-27 16:22:51 -0500

    Attempting to add first sound

--- whatdavesaid-ios/ClipsListViewController.swift
+++ whatdavesaid-ios/ClipsListViewController.swift
@@ -8,12 +8,42 @@
 
 import Foundation
 import UIKit
+import AVFoundation
 
 class ClipsListViewController : UIViewController {
+    
+    @IBOutlet weak var clipsList: UITableView!
+    
+    var clips = []
+    
+    var audioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.clips.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var sound = self.clips[indexPath.row]
+        var cell = UITableViewCell()
+        cell.textLabel!.text = sound.name
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        var path = NSBundle.mainBundle().pathForResource("black-metal", ofType: "mp3")
+        var url = NSURL.fileURLWithPath(path!)
+        
+        do {
+            self.audioPlayer = try AVAudioPlayer(contentsOfURL: url)
+            self.audioPlayer.play()
+            
+        } catch let error as NSError {
+            print("AV Sound Error: \(error.localizedDescription)")
+        }
+    }
     
 }
